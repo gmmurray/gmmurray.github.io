eb07e2dd1b7950990963a7ce01fcca3afcdb3ecd
gmmurray <trinilime123@live.com>
2020-01-11 20:27:27 -0500

    add title to website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React, { Component, Fragment } from 'react';
+import { Helmet } from "react-helmet";
 import Intro from '../components/intro';
 import About from '../components/about/about';
 import Experiences from '../components/experiences/experiences';
 import Projects from '../components/projects/projects';
 import Navigation from '../components/navigation';
 import Footer from '../components/footer';
-import {spyScrolling} from '../helperFunctions';
+import { spyScrolling } from '../helperFunctions';
 
 export default class IndexPage extends Component {
     componentDidMount() {
@@ -15,6 +16,10 @@ export default class IndexPage extends Component {
     render() {
         return (
             <Fragment>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Jacksonville, FL Computing Student</title>
+                </Helmet>
                 <Intro />
                 <Navigation />
                 <About />
