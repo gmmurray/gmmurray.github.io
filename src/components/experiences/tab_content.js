6d8871123e6d987658be1527f2d30b439409ae05
gmmurray <trinilime123@live.com>
2020-05-06 23:21:58 -0400

    update after graduation

--- src/components/experiences/tab_content.js
+++ src/components/experiences/tab_content.js
@@ -1,49 +1,85 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react"
 
 const TabContent = [
-    {
-        name: 'SS&C Advent',
-        content:
-            <Fragment>
-                <div className="animated fadeIn">
-                    <h4 className="title is-4 animated fadeIn">Software Development Internship</h4>
-                    <h5 className="subtitle is-5">January 2020 - Present</h5>
-                    <p className="is-size-4-desktop">-Research and debug existing codebase as a member of the Product Development team</p>
-                    {/* <p className="is-size-4-desktop">-Submit bugfixes to development and production code</p> */}
-                    <p className="is-size-4-desktop">-Enhance documentation for existing codebase</p>
-                    <p className="is-size-4-desktop">-Assist in implementation and testing of new features</p>
-                    <p className="is-size-4-desktop">-Follow Agile methodologies with daily standup and project board</p>
-                </div>
-            </Fragment>
-    },
-    {
-        name: 'University of North Florida',
-        content: 
-            <Fragment>
-                <div className="animated fadeIn">
-                    <h4 className="title is-4">Lab Assistant/Tutor</h4>
-                    <h5 className="subtitle is-5">August 2019 - December 2019</h5>
-                    <p className="is-size-4-desktop">-Provide academic assistance to university students on the subjects of C, C++, Java, Computational Structures and Data Structures</p>
-                    <p className="is-size-4-desktop">-Engage with students of varying academic levels (first to fourth year) to supplement their understanding of their current classes</p>
-                    <p className="is-size-4-desktop">-Responsible for computer lab lock-up procedures at the end of the night</p>
-                </div>
-            </Fragment>
-    },
-    {
-        name: 'Five STAR Veterans Center',
-        content: 
-            <Fragment>
-                <div className="animated fadeIn">
-                    <h4 className="title is-4">Web Consultant</h4>
-                    <h5 className="subtitle is-5">June 2019 - Present</h5>
-                    <p className="is-size-4-desktop">-Planned and designed potential website updates</p>
-                    <p className="is-size-4-desktop">-Implemented updates after receiving approval from Chief Administrative Officer</p>
-                    <p className="is-size-4-desktop">-Coordinated changes with designated company employees</p>
-                    <p className="is-size-4-desktop">-Oversaw whole product lifecycle</p>
-                    <p className="is-size-4-desktop">-Received feedback of increased traffic to the website</p>
-                </div>
-        </Fragment>
-    },
-];
+  {
+    name: "SS&C Advent",
+    content: (
+      <Fragment>
+        <div className="animated fadeIn">
+          <h4 className="title is-4 animated fadeIn">
+            Contract Software Engineer
+          </h4>
+          <h5 className="subtitle is-5">
+            May 2020 - Present (Intern January - May)
+          </h5>
+          <p className="is-size-4-desktop">
+            -Develop frontend solutions with React/Redux
+          </p>
+          {/* <p className="is-size-4-desktop">-Submit bugfixes to development and production code</p> */}
+          <p className="is-size-4-desktop">
+            -Build backend code using C# in ASP.NET in addition to SQL and
+            MongoDB
+          </p>
+          <p className="is-size-4-desktop">
+            -Oversee my code from planning and research, through development and
+            testing, to production deployment
+          </p>
+          <p className="is-size-4-desktop">
+            -Participate in an Agile team and contribute to code review
+          </p>
+          <p className="is-size-4-desktop">-Utilize Git version control</p>
+        </div>
+      </Fragment>
+    ),
+  },
+  {
+    name: "University of North Florida",
+    content: (
+      <Fragment>
+        <div className="animated fadeIn">
+          <h4 className="title is-4">Lab Assistant/Tutor</h4>
+          <h5 className="subtitle is-5">August 2019 - December 2019</h5>
+          <p className="is-size-4-desktop">
+            -Provide academic assistance to university students on the subjects
+            of C, C++, Java, Computational Structures and Data Structures
+          </p>
+          <p className="is-size-4-desktop">
+            -Engage with students of varying academic levels (first to fourth
+            year) to supplement their understanding of their current classes
+          </p>
+          <p className="is-size-4-desktop">
+            -Responsible for computer lab lock-up procedures at the end of the
+            night
+          </p>
+        </div>
+      </Fragment>
+    ),
+  },
+  {
+    name: "Five STAR Veterans Center",
+    content: (
+      <Fragment>
+        <div className="animated fadeIn">
+          <h4 className="title is-4">Web Consultant</h4>
+          <h5 className="subtitle is-5">June 2019 - May 2020</h5>
+          <p className="is-size-4-desktop">
+            -Planned and designed potential website updates
+          </p>
+          <p className="is-size-4-desktop">
+            -Implemented updates after receiving approval from Chief
+            Administrative Officer
+          </p>
+          <p className="is-size-4-desktop">
+            -Coordinated changes with designated company employees
+          </p>
+          <p className="is-size-4-desktop">-Oversaw whole product lifecycle</p>
+          <p className="is-size-4-desktop">
+            -Received feedback of increased traffic to the website
+          </p>
+        </div>
+      </Fragment>
+    ),
+  },
+]
 
 export default TabContent
--- src/components/projects/other_projects_list.js
+++ src/components/projects/other_projects_list.js
@@ -1,46 +1,52 @@
 const OtherProjectsList = [
-    {
-        title: 'Entry Level Interview Study Guide',
-        description: 'Web based study guide for entry level software developer interviews. I made it for myself and my friends as a way to consolidate a lot of information and be a good starting point for preparing for an interview.',
-        tags: ['React'],
-        repo: 'https://github.com/gmmurray/interview-study-guide',
-        link: 'https://gregmurray.org/interview-study-guide'
-    },
-    {
-        title: 'Simple Browser Game',
-        description: 'Simple vanilla JavaScript browser game built following the tutorial found on the Lost Decade Games website (link is on the repo page). There are only two levels and I plan to add a few more.',
-        tags: ['Vanilla JavaScript'],
-        repo: 'https://github.com/gmmurray/SimpleHTMLGame-Goblins',
-        link: 'https://gregmurray.org/SimpleHTMLGame-Goblins'
-    },
-    {
-        title: 'React TicTacToe Demo',
-        description: 'React Tic Tac Toe game from the official React Tutorial found on the React website. My future goal is to apply some additional styling to improve the appearance of the game.',
-        tags: ['React'],
-        repo: 'https://github.com/gmmurray/ReactTicTacToeDemo',
-        link: 'https://gregmurray.org/ReactTicTacToeDemo'
-    },
-    {
-        title: 'React Tutorial',
-        description: 'Helpful tutorial I found to help learn some React concepts which I ended up using to build this site. Link to the guide I followed is on the repo.',
-        tags: ['React'],
-        repo: 'https://github.com/gmmurray/tania-react-tutorial',
-        link: ''
-    },
-    {
-        title: 'Sass Tutorial',
-        description: 'I find traditional CSS to be time consuming and a bit frustrating, so I was happy to find a great guide that explains a couple Sass (specifically SCSS) techniques then shows them off in a full tutorial resulting in a pretty nice looking resume website. I followed a YouTube video to build this, the link to which can be found in the repository.',
-        tags: ['Sass'],
-        repo: 'https://github.com/gmmurray/SassTutorial',
-        link: 'https://gregmurray.org/SassTutorial/dist'
-    },
-    {
-        title: 'Angular Tour of Heroes',
-        description: 'Before learning React, my first exposure to a full front end JS framework was Angular. I found the documentation on Angular\'s website very helpful, beginning with the Angular Tour of Heroes which is what I followed for this project.',
-        tags: ['Angular'],
-        repo: 'https://github.com/gmmurray/angular-tour-of-heroes',
-        link: 'https://gregmurray.org/angular-tour-of-heroes'
-    },
-];
+  {
+    title: "Entry Level Interview Study Guide",
+    description:
+      "Web based study guide for entry level software developer interviews. I made it for myself and my friends as a way to consolidate a lot of information and be a good starting point for preparing for an interview.",
+    tags: ["ReactJS"],
+    repo: "https://github.com/gmmurray/interview-study-guide",
+    link: "https://gregmurray.org/interview-study-guide",
+  },
+  {
+    title: "Super Secret",
+    description:
+      "Simple password generator app that utilizes the random.org API to generate truly random strings. I built a ReactJS app that allows you to choose a few different parameters to create a complex password.",
+    tags: ["ReactJS", "React-Bootstrap"],
+    repo: "https://github.com/gmmurray/super-secret",
+    link: "https://gregmurray.org/super-secret",
+  },
+  {
+    title: "Simple Browser Game",
+    description:
+      "Simple vanilla JavaScript browser game built following the tutorial found on the Lost Decade Games website (link is on the repo page). There are only two levels and I plan to add a few more.",
+    tags: ["Vanilla JavaScript"],
+    repo: "https://github.com/gmmurray/SimpleHTMLGame-Goblins",
+    link: "https://gregmurray.org/SimpleHTMLGame-Goblins",
+  },
+  {
+    title: "React TicTacToe Demo",
+    description:
+      "React Tic Tac Toe game from the official React Tutorial found on the React website. My future goal is to apply some additional styling to improve the appearance of the game.",
+    tags: ["ReactJS"],
+    repo: "https://github.com/gmmurray/ReactTicTacToeDemo",
+    link: "https://gregmurray.org/ReactTicTacToeDemo",
+  },
+  {
+    title: "React Tutorial",
+    description:
+      "Helpful tutorial I found to help learn some React concepts which I ended up using to build this site. Link to the guide I followed is on the repo.",
+    tags: ["ReactJS"],
+    repo: "https://github.com/gmmurray/tania-react-tutorial",
+    link: "",
+  },
+  {
+    title: "Sass Tutorial",
+    description:
+      "I find traditional CSS to be time consuming and a bit frustrating, so I was happy to find a great guide that explains a couple Sass (specifically SCSS) techniques then shows them off in a full tutorial resulting in a pretty nice looking resume website. I followed a YouTube video to build this, the link to which can be found in the repository.",
+    tags: ["Sass"],
+    repo: "https://github.com/gmmurray/SassTutorial",
+    link: "https://gregmurray.org/SassTutorial/dist",
+  },
+]
 
 export default OtherProjectsList
