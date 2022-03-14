447c54359bf8e55258da870d28c4e1e0a3446995
gmmurray <trinilime123@live.com>
2022-03-13 23:21:30 -0400

    implemented new sprites

--- src/game/assetDefinitions/sprites.ts
+++ src/game/assetDefinitions/sprites.ts
@@ -2,6 +2,24 @@ import { SCALE } from '../constants';
 import { SpriteDefinition } from '../types/assetDefinitions';
 import { getSpriteSource } from '../helpers/getAssetSource';
 
+const createPlayerSpriteDefinition = (source: string): SpriteDefinition => ({
+  key: 'player',
+  source: getSpriteSource(`${source}_spritesheet`),
+  frameWidth: 32,
+  frameHeight: 32,
+  walkingAnimationMapping: 0,
+  scale: SCALE,
+});
+
+export const playerCharacterOptions: SpriteDefinition[] = [
+  createPlayerSpriteDefinition('xion'),
+  createPlayerSpriteDefinition('irabel'),
+  createPlayerSpriteDefinition('zaya'),
+  createPlayerSpriteDefinition('orryn'),
+];
+
+const randomCharacterNumber = Math.floor(Math.random() * 4);
+
 export const gregSpriteDefinition: SpriteDefinition = {
   key: 'greg',
   source: getSpriteSource('greg_spritesheet'),
@@ -11,29 +29,23 @@ export const gregSpriteDefinition: SpriteDefinition = {
   scale: 2,
 };
 
-export const playerSpriteDefinition: SpriteDefinition = {
-  key: 'player',
-  source: getSpriteSource('player_spritesheet'),
-  frameWidth: 32,
-  frameHeight: 32,
-  walkingAnimationMapping: 0,
-  scale: SCALE,
-};
+export const playerSpriteDefinition: SpriteDefinition =
+  playerCharacterOptions[randomCharacterNumber];
 
 export const greyCatSpriteDefinition: SpriteDefinition = {
   key: 'grey-cat',
   source: getSpriteSource('grey_cat_spritesheet'),
-  frameWidth: 45,
-  frameHeight: 68,
+  frameWidth: 32,
+  frameHeight: 32,
   walkingAnimationMapping: 0,
-  scale: 1,
+  scale: 2,
 };
 
 export const whiteCatSpriteDefinition: SpriteDefinition = {
   key: 'white-cat',
   source: getSpriteSource('white_cat_spritesheet'),
-  frameWidth: 45,
-  frameHeight: 68,
+  frameWidth: 32,
+  frameHeight: 32,
   walkingAnimationMapping: 0,
-  scale: 1,
+  scale: 2,
 };
--- src/game/scenes/LevelOne.ts
+++ src/game/scenes/LevelOne.ts
@@ -12,8 +12,8 @@ export class LevelOne extends LevelScene {
   constructor() {
     super('level-one');
     this.levelNumber = 1;
-    this.startingGridCoordinates.x = 15;
-    this.startingGridCoordinates.y = 98;
+    this.startingGridCoordinates.x = 24;
+    this.startingGridCoordinates.y = 51;
     this.mapDefinition = levelOneMapDefinition;
   }
 
